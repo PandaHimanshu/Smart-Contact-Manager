c3908a85bec3b49196b547cd0fa3cba1a7089884
himanshu <pandahimanshu707@gmail.com>
2024-08-05 22:52:48 +0530

    first commit

---
+++ src/main/java/com/scm/SmartControlManager/SmartControlManagerApplication.java
@@ -0,0 +1,13 @@
+package com.scm.SmartControlManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartControlManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartControlManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scm/SmartControlManager/SmartControlManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.SmartControlManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartControlManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
