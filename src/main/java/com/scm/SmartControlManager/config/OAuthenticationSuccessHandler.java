a5854cb305d91906dfbfb6f01f7f11ca3e146478
himanshu <pandahimanshu707@gmail.com>
2024-08-12 15:04:43 +0530

    login with Github completed

--- src/main/java/com/scm/SmartControlManager/config/OAuthenticationSuccessHandler.java
+++ src/main/java/com/scm/SmartControlManager/config/OAuthenticationSuccessHandler.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -34,35 +35,57 @@ public class OAuthenticationSuccessHandler implements AuthenticationSuccessHandl
         
                 logger.info("OAuthenticationSuccessHandler");
 
-                DefaultOAuth2User user =(DefaultOAuth2User)authentication.getPrincipal();
-                // logger.info(user.getName());
-                // user.getAttributes().forEach((key,value)->{
-                //     logger.info("{} => {}",key,value);
-                // });
+                //Identify the provider github or google
+                var oauth2AuthenticationToken=(OAuth2AuthenticationToken)authentication;
+                String authorizedClientRegistrationId = oauth2AuthenticationToken.getAuthorizedClientRegistrationId();
 
+                var oauthUser =(DefaultOAuth2User)authentication.getPrincipal();
 
-                //save data to database
-                String email = user.getAttribute("email").toString();
-                String name = user.getAttribute("name").toString();
-                String picture = user.getAttribute("picture").toString();
+                //   oauthUser.getAttributes().forEach((key,value)->{
+                //         logger.info("{} => {}",key,value);
+                //     });
 
-                User user1 = new User();
-                user1.setEmail(email);
-                user1.setName(name);
-                user1.setProfilePic(picture);
-                user1.setPassword("password");
-                user1.setUserId(UUID.randomUUID().toString());
-                user1.setProvider(Providers.GOOGLE);
-                user1.setProviderUserId(user.getName());
-                user1.setEnabled(true);
-                user1.setEmailVerified(true);
-                user1.setRoleList(List.of(AppConstants.ROLE_USER));
-                user1.setAbout("This account is created via Google");
+                User user = new User();
+                user.setUserId(UUID.randomUUID().toString());
+                user.setRoleList(List.of(AppConstants.ROLE_USER));
+                user.setEmailVerified(true);
+                user.setEnabled(true);
+                user.setPassword("password");
 
-                User user2 = userRepository.findByEmail(email).orElse(null);
+                //for google
+                if (authorizedClientRegistrationId.equalsIgnoreCase("google")) {
+                    user.setEmail(oauthUser.getAttribute("email").toString());
+                    user.setProfilePic(oauthUser.getAttribute("picture").toString());
+                    user.setName(oauthUser.getAttribute("name").toString());
+                    user.setProviderUserId(oauthUser.getName());
+                    user.setAbout("this user is created by google");
+                    user.setProvider(Providers.GOOGLE);
+                    
+                }
+
+                //for github
+                else if(authorizedClientRegistrationId.equalsIgnoreCase("github")){
+                    String email = oauthUser.getAttribute("email") != null? 
+                    oauthUser.getAttribute("email").toString():
+                    oauthUser.getAttribute("login").toString()+"@github.com";
+                    String picture = oauthUser.getAttribute("avatar_url").toString();
+                    String name =oauthUser.getAttribute("login").toString();
+                    String providerUserId =oauthUser.getName();
+
+                    user.setEmail(email);
+                    user.setName(name);
+                    user.setProfilePic(picture);
+                    user.setProviderUserId(providerUserId);
+                    user.setAbout("This account is created by Github");
+                    user.setProvider(Providers.GITHUB);
+                }
+                else{
+                    logger.info("Unknown provider");
+                }
+                User user2 = userRepository.findByEmail(user.getEmail()).orElse(null);
                 if (user2==null) {
-                    userRepository.save(user1);
-                    logger.info("User Saved : "+email);
+                    userRepository.save(user);
+                    logger.info("User Saved : "+user.getEmail());
                 }
 
 
